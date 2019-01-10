fea623da96150f5ad053b0b937abcd76f3cc9d17
Leesel Fraser <lfraser@teacherspayteachers.com>
2019-01-10 17:49:02 -0500

    removed top repo and branch lines,rmvd token and added tag in place of branch

--- emaillookup.sh
+++ emaillookup.sh
@@ -1,13 +1,11 @@
 email=$(git log --format="%ae" HEAD^!)
-repo='tpt-frontend'
-branch='my-awesome-branch'
 
 name=$(curl \
-  -d "token=xoxp-517926574081-518426450451-518376735460-5a1042f12e921349c2ef081576744e06&email=$email" \
+  -d "token=$SLACK_TOKEN&email=$email" \
   https://slack.com/api/users.lookupByEmail | jq -r .user.name)
 
 echo "The value of name is ${name}"
 
 curl -X POST -H 'Content-type: application/json' \
-  --data "{\"text\":\"The build for $repo from the $branch branch is ready for $email @$name\",\"link_names\":true}" \
+  --data "{\"text\":\"The build for $repo from the $tag branch is ready for $email @$name\",\"link_names\":true}" \
   https://hooks.slack.com/services/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
