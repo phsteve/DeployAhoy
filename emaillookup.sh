20bd47d767a01dd70836e299ee148e2d2cfe42ce
Leesel Fraser <lfraser@teacherspayteachers.com>
2019-01-14 11:01:07 -0500

    emaillookup.sh

--- emaillookup.sh
+++ emaillookup.sh
@@ -4,8 +4,13 @@ name=$(curl \
   -d "token=$SLACK_TOKEN&email=$email" \
   https://slack.com/api/users.lookupByEmail | jq -r .user.name)
 
-echo "The value of name is ${name}"
+
+if [ "$name" != "null" ]
+then message="The build for $repo from the $tag branch is ready for @$name"
+else message="The build for $repo from the $tag branch is ready for ${email}. If you would like to be @ mentioned, please change your git email to your tpt email."
+fi
 
 curl -X POST -H 'Content-type: application/json' \
-  --data "{\"text\":\"The build for $repo from the $tag branch is ready for $email @$name\",\"link_names\":true}" \
-  https://hooks.slack.com/services/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
+  --data "{\"text\":\"$message\",\"link_names\":true}" \
+https://hooks.slack.com/services/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
+
