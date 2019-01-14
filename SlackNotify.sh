d379391bc3928173def5f77342b0be007b207349
Leesel Fraser <lfraser@teacherspayteachers.com>
2019-01-14 15:47:45 -0500

    Changed slack url to a variable

--- SlackNotify.sh
+++ SlackNotify.sh
@@ -4,7 +4,6 @@ name=$(curl \
   -d "token=$SLACK_TOKEN&email=$email" \
   https://slack.com/api/users.lookupByEmail | jq -r .user.name)
 
-
 if [ "$name" != "null" ]
 then message="The build for $repo from the $tag branch is ready for @$name"
 else message="The build for $repo from the $tag branch is ready for ${email}. If you would like to be @ mentioned, please change your git email to your tpt email."
@@ -12,5 +11,5 @@ fi
 
 curl -X POST -H 'Content-type: application/json' \
   --data "{\"text\":\"$message\",\"link_names\":true}" \
-https://hooks.slack.com/services/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
+  "$SLACK_CHANNEL_URL"
 
